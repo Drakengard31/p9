88330d104a5b528410cfa7bad8b76d9014797da8
Drakengard31 <lucdimitri31@gmail.com>
2025-06-19 18:54:16 +0200

    correction d'un bug le Problème : Invalid prop 'type' of value 'email' supplied to 'Field', expected one of [1,2]               Cause : Incompatibilité entre les types attendus par Field et ceux passés par Form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,63 +4,116 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
+const mockContactApi = () => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve({ success: true });
+    }, 500);
+});
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState(false);
-  const sendContact = useCallback(
-    async (evt) => {
-      evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
-      try {
-        await mockContactApi();
-        setSending(false);
-      } catch (err) {
-        setSending(false);
-        onError(err);
-      }
-    },
-    [onSuccess, onError]
-  );
-  return (
-    <form onSubmit={sendContact}>
-      <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
-          <Select
-            selection={["Personel", "Entreprise"]}
-            onChange={() => null}
-            label="Personel / Entreprise"
-            type="large"
-            titleEmpty
-          />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
-            {sending ? "En cours" : "Envoyer"}
-          </Button>
-        </div>
-        <div className="col">
-          <Field
-            placeholder="message"
-            label="Message"
-            type={FIELD_TYPES.TEXTAREA}
-          />
-        </div>
-      </div>
-    </form>
-  );
+    const [sending, setSending] = useState(false);
+    const [formData, setFormData] = useState({
+        lastname: "",
+        firstname: "",
+        email: "",
+        type: "Personel",
+        message: ""
+    });
+
+    const sendContact = useCallback(
+        async (evt) => {
+            evt.preventDefault();
+            setSending(true);
+            try {
+                await mockContactApi();
+                onSuccess(); // Appel explicite de onSuccess
+            } catch (err) {
+                onError(err);
+            } finally {
+                setSending(false);
+            }
+        },
+        [onSuccess, onError]
+    );
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    return (
+        <form onSubmit={sendContact}>
+            <div className="row">
+                <div className="col">
+                    <Field
+                        name="lastname"
+                        value={formData.lastname}
+                        onChange={handleInputChange}
+                        label="Nom"
+                        data-testid="lastname-field"
+                        required
+                    />
+                    <Field
+                        name="firstname"
+                        value={formData.firstname}
+                        onChange={handleInputChange}
+                        label="Prénom"
+                        data-testid="firstname-field"
+                        required
+                    />
+                    <Select
+                        name="type"
+                        value={formData.type}
+                        onChange={handleInputChange}
+                        selection={["Personel", "Entreprise"]}
+                        label="Personel / Entreprise"
+                        data-testid="type-select"
+                        type="large"
+                        titleEmpty
+                    />
+                    <Field
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        label="Email"
+                        placeholder="votre@email.com"
+                        data-testid="email-field"
+                        required
+                    />
+                    <Button
+                        type={BUTTON_TYPES.SUBMIT}
+                        disabled={sending}
+                        data-testid="submit-button"
+                    >
+                        {sending ? "En cours" : "Envoyer"}
+                    </Button>
+                </div>
+                <div className="col">
+                    <Field
+                        name="message"
+                        value={formData.message}
+                        onChange={handleInputChange}
+                        label="Message"
+                        type={FIELD_TYPES.TEXTAREA} // utilise la constante
+                        data-testid="message-field"
+                    />
+                </div>
+            </div>
+        </form>
+    );
 };
 
 Form.propTypes = {
-  onError: PropTypes.func,
-  onSuccess: PropTypes.func,
-}
+    onError: PropTypes.func,
+    onSuccess: PropTypes.func,
+};
 
 Form.defaultProps = {
-  onError: () => null,
-  onSuccess: () => null,
-}
+    onError: () => null,
+    onSuccess: () => null,
+};
 
 export default Form;
